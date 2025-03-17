eef95cc48da14682820a856ce982851c78c83ebd
Carlos Stiven Giraldo Rodriguez <1109117326@u.icesi.edu.co>
2025-03-17 09:56:53 -0500

    ejercicio de paises

--- tipos.js
+++ tipos.js
@@ -1,3 +1,17 @@
-console.log('Hola mundo!!');
-let nombre = prompt('Ingresa tu nombre');
-console.log("Hola," + nombre + "!");
+let pais = prompt("Ingrese su país")
+let idioma = ""
+
+if (pais === "colombia") {
+    idioma = "Español";
+} else if (pais === "italia") {
+    idioma = "francés"
+} else if (pais === "alemania") {
+    idioma = "alemán"
+} else if (pais === "japón") {
+    idioma = "japones"
+} else if (pais === "rusia") {
+    idioma = "ruso"
+} else {
+    console.log("No existe el país!");
+}
+console.log("En " + pais + " se habla: " + idioma);
