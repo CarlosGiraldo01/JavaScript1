ec0878075bff22eb3b6f49d74538f9f511aa6af6
Carlos Stiven Giraldo Rodriguez <1109117326@u.icesi.edu.co>
2025-03-21 17:34:12 -0500

    Lo de la clase

---
+++ punto3.js
@@ -0,0 +1,16 @@
+/* C1 ES CALIFICACIÓN*/
+let c1 = 5;
+let c2 = 5;
+let c3 = 5;
+let c4 = 5;
+let c5 = 5;
+
+let promedio = (c1+c2+c3+c4+c5)/5;
+
+console.log(promedio);
+
+if (promedio >= 3) {
+    console.log("Usted a aprobado");
+} else {
+    console.log("Usted a reprobado");
+}
